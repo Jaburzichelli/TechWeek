13ac5640ee7d98a6ff5e1603d70d6dcf44ea9dd4
Donassan <enzo.donassan@gmail.com>
2025-10-24 10:44:13 -0300

    .

---
+++ sistemas-reservas-senac/js/core.js
@@ -0,0 +1,17 @@
+/**
+ * Configurações globais do sistema
+ */
+'use strict';
+
+const CONFIG = {
+    APP_NAME: 'Sistema de Reservas SENAC',
+    VERSION: '1.0.0',
+    API_URL: '/api',
+    STORAGE_KEY: 'senac_reservations_data',
+    WORKING_HOURS: {
+        start: '08:00',
+        end: '22:00'
+    },
+    SLOT_DURATION: 60,
+    TOAST_DURATION: 4000
+};
